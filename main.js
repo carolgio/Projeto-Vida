38424949f09daaaf224f862ff1e71b504ac70ba2
carolgio <108530479+carolgio@users.noreply.github.com>
2024-04-29 10:56:05 -0300

    s

--- main.js
+++ main.js
@@ -4,14 +4,14 @@ const textos = document.querySelectorAll(".aba-conteudo");
 for(let i=0;i <botoes.length;i++){
     botoes[i].onclick = function(){
 
-        for(let j=0,j<botoes.length;j++){
+        for(let j=0;j < botoes.length; j++){
             botoes[j].classList.remove("ativo");
         }
 
         botoes[i].classList.add("ativo");
         textos[i].classList.add("ativo");
     }
-    
+    console.log();
 }
 
 const contadores = document.querySelectorAll(".contador");
@@ -20,7 +20,7 @@ const tempoObjetivo2 = new Date("2024-12-05T00:00:00");
 const tempoObjetivo3 = new Date("2024-12-30T00:00:00");
 const tempoObjetivo4 = new Date("2024-02-01T00:00:00");
 
-const tempos = [tempoObjetivo1,tempoObjetivo2,tempoObjetivo3,tempoObjetivo4]
+const tempos = [tempoObjetivo1,tempoObjetivo2,tempoObjetivo3,tempoObjetivo4];
 
 function calculaTempo(tempoObjetivo){
     let tempoAtual = new Date();
@@ -55,4 +55,5 @@ function comecaCronometro(){
     atualizaCronometro();
     setInterval(atualizaCronometro,1000);
 }
-//
+
+//comecaCronometro();
